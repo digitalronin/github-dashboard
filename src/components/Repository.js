98771342982a4b555c3a6388c4496603ba1e8e6e
David Salgado <david@digitalronin.com>
2019-05-11 17:02:45 +0100

    Query the github api and render repo data

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const REPO_QUERY = gql`
+  {
+    repository(name: "cloud-platform", owner: "ministryofjustice") {
+      hasIssuesEnabled
+      owner {
+        login
+      }
+      name
+      issues {
+        totalCount
+      }
+    }
+  }
+`;
 
 class Repository extends Component {
   render() {
     return (
-      <div className='repository'>
-        <h1>I am a repository</h1>
-      </div>
+      <Query query={REPO_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+          const { repository } = data;
+          return (
+            <div className='repository'>
+              <h1>
+                {repository.owner.login}/
+                {repository.name}
+              </h1>
+              <h2>
+                Issues: {repository.issues.totalCount}
+              </h2>
+            </div>
+          );
+        }}
+      </Query>
     );
   }
-
 }
 
 export default Repository;
