f585c4b402184fb3b9d4e82db097b8499c4cc1dd
David Salgado <david@digitalronin.com>
2019-05-11 14:45:49 +0100

    Replace default content with Repository component

---
+++ src/components/Repository.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Repository extends Component {
+  render() {
+    return (
+      <div className='repository'>
+        <h1>I am a repository</h1>
+      </div>
+    );
+  }
+
+}
+
+export default Repository;
