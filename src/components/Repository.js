995f688584e8c6ea29f191e1c1fc2826e25de802
David Salgado <david@digitalronin.com>
2019-05-12 12:22:01 +0100

    Fetch issues by date, using graphql variables

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -3,33 +3,56 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const REPO_QUERY = gql`
-  {
-    repository(name: "cloud-platform", owner: "ministryofjustice") {
-      hasIssuesEnabled
-      owner {
-        login
+query ($owner: String!, $name:String!, $startDate: DateTime!, $after: String) {
+  repository(name: $name, owner: $owner) {
+    issues(filterBy: {since: $startDate}, orderBy: {field: CREATED_AT, direction: ASC}, first: 50, after: $after) {
+      pageInfo {
+        hasNextPage
+        endCursor
       }
-      name
-      issues {
-        totalCount
+      totalCount
+      nodes {
+        state
+        title
+        createdAt
+        closedAt
+        labels(first: 100) {
+          nodes {
+            name
+          }
+        }
       }
     }
   }
+}
 `;
+
+const owner = "ministryofjustice"; // repository owner
+const name = "cloud-platform";  // repository name
+const startDate = "2019-05-01T00:00:00Z";  // **Updated At** timestamp of the issue - could have been created earlier
+const after = "Y3Vyc29yOnYyOpK5MjAxOS0wNS0wOFQxNTo1NDowMCswMTowMM4aVP_g";  // GraphQL endCursor for paginated results
+
+const VARIABLES = {
+  owner,
+  name,
+  startDate,
+  after
+};
 
 class Repository extends Component {
   render() {
     return (
-      <Query query={REPO_QUERY}>
+      <Query query={REPO_QUERY} variables={VARIABLES}>
         {({ loading, error, data }) => {
           if (loading) return <div>Fetching</div>;
           if (error) return <div>Error</div>;
+          console.log(data);
           const { repository } = data;
           return (
             <div className='repository'>
               <h1>
-                {repository.owner.login}/
-                {repository.name}
+                {owner}/
+                {name}
               </h1>
               <h2>
                 Issues: {repository.issues.totalCount}
